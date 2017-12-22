387ad6442a01890aa5e4d82b09b67cc58efce931
Gentrit Sahiti <genti_student@outlook.com>
2017-12-22 09:40:40 +0100

    Add project files.

---
+++ AdressBookApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AdressBookApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
